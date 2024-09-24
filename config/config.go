97d92412ee3a7f1d1e64fbddb29c43255764b85f
Abir835 <abirhasan.raj.bd@gmail.com>
2024-09-24 12:10:26 +0600

    [change] --> config file change

--- config/config.go
+++ config/config.go
@@ -1,19 +1,19 @@
 package config
 
-//type DBConfig struct {
-//	Host                string `json:"host" validate:"required"`
-//	Port                int    `json:"port" validate:"required"`
-//	Name                string `json:"name" validate:"required"`
-//	User                string `json:"user" validate:"required"`
-//	Pass                string `json:"pass" validate:"required"`
-//	MaxIdleTimeInMinute int    `json:"max_idle_time_in_minute" validate:"required"`
-//	EnableSSLMode       bool   `json:"enable_ssl_mode"`
-//}
+type DBConfig struct {
+	Host                string `json:"host"                    validate:"required"`
+	Port                int    `json:"port"                    validate:"required"`
+	Name                string `json:"name"                    validate:"required"`
+	User                string `json:"user"                    validate:"required"`
+	Pass                string `json:"pass"                    validate:"required"`
+	MaxIdleTimeInMinute int    `json:"max_idle_time_in_minute" validate:"required"`
+	EnableSSLMode       bool   `json:"enable_ssl_mode"`
+}
 
-//type DB struct {
-//	Read  string `json:"read" validate:"required"`
-//	Write string `json:"write" validate:"required"`
-//}
+type DB struct {
+	Read  DBConfig `json:"read"  validate:"required"`
+	Write DBConfig `json:"write" validate:"required"`
+}
 
 type Mode string
 
@@ -21,11 +21,11 @@ const DebugMode = Mode("debug")
 const ReleaseMode = Mode("release")
 
 type Config struct {
-	Mode        Mode   `json:"mode"                       validate:"required"`
-	ServiceName string `json:"service_name"               validate:"required"`
-	HttpPort    int    `json:"http_port"                  validate:"required"`
-	JwtSecret   string `json:"jwt_secret"                 validate:"required"`
-	//DB                DB     `json:"db"                         validate:"required"`
+	Mode              Mode   `json:"mode"                       validate:"required"`
+	ServiceName       string `json:"service_name"               validate:"required"`
+	HttpPort          int    `json:"http_port"                  validate:"required"`
+	JwtSecret         string `json:"jwt_secret"                 validate:"required"`
+	DB                DB     `json:"db"                         validate:"required"`
 	MigrationSource   string `json:"migrations"                 validate:"required"`
 	RmqQueuePrefix    string `json:"rmq_queue_prefix"           validate:"required"`
 	RabbitmqURL       string `json:"rmq_url"                    validate:"required"`
